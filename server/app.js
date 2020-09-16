f91bc74d20967834456ad080f27330fa736ad7fc
Ritik Gupta <guptaritik349@gmail.com>
2020-09-16 16:55:06 +0530

    Started Server work

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const path = require("path");
 const cors = require("cors");
 
 const app = express();
@@ -10,7 +11,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
+app.post("/signup", function (req, res) {
+  var filePath = path.join(__dirname, "../client/index.html");
+  // res.sendFile(filePath);
+  res.redirect("http://localhost:3000/index.html");
+});
+
 app.listen(3001, () => {
   console.log("Server is running on port 3000");
-})
+});
 

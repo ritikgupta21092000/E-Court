57fa6f218b42061d0e4a11fde144df6da999522f
Ritik Gupta <guptaritik349@gmail.com>
2020-12-09 01:04:34 +0530

    Added Procfile

--- server/app.js
+++ server/app.js
@@ -892,6 +892,6 @@ app.post("/updateCase", function (req, res) {
 
 });
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log("Server is running on port 5000");
 });
